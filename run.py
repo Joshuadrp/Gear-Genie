e29352773e08cdb61db1f4c67b4387b5e2460e44
Joshua Rodriguez <joshuadrp@hotmail.com>
2024-05-14 14:05:30 +0000

    Initial commit, fetch weather data from api upon users input

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import requests
+
+api_key = '6a3f2cc91e7c4aa8d6506c5f08f260e4'
+
+user_location = input('Please state the place you want to go:\n')
+
+weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={user_location}&units=metric&APPID={api_key}")
+
+weather = weather_data.json()['weather'][0]['main']
+temp = weather_data.json()['main']['temp']
+
+# print(weather_data.json())
+
+
