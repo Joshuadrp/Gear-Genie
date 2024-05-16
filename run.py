bade28be827f82039bbbf4a0fff943f78f61af5f
Joshua Rodriguez <joshuadrp@hotmail.com>
2024-05-16 10:00:47 +0000

    add display_weather_basic function to display weather and ask for user input

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ def get_weather(location):
     error_code = weather_data.json()['cod']
 
     # print(weather, temp)
-    print(error_code)
+    # print(error_code)
     # print(weather_data.json())
 
     return weather, temp, error_code
@@ -22,13 +22,13 @@ def location_input():
     Asks for users desired location and displays weather data.
     """
     location = input("Please enter desired location: ")
-    print("""Location received.\nNow, please enter which type of outdoor activity in order to advise you wisely.\nYou can choose the following:\n1.Climbing\n2.Hiking.\n""")
     return location
 
 def activity_input():
     """
     Gets users location and acivity
     """
+    print("Location received.\nNow, please enter which type of outdoor activity in order to advise you wisely.\nYou can choose the following:\n1.Climbing\n2.Hiking.\n")
     while True:
         
         activity = input("Please enter outdoor activity: ")
@@ -55,15 +55,31 @@ def activity_input():
             return activity, climbing
         else: 
             print("Please choose one of the activities above.\n")
+
+def display_weather_basic(weather, temp):
+    """
+    Displays basic weather info for user and let user choose to continue with app or terminate it.
+    """
+    print(f"Weather is currently {weather}, with temperature of {temp}.\nAfter reviewing the weather data, do you wish to continue?\n1.Yes\n2.No")
+    while True:
+        user_agree = input("Please choose: ")
+        if(user_agree == "1"):
+            activity_input()
+        elif(user_agree == "2"):
+            print("Program will be terminated.")
+            return
+        else:
+            print("Please select a valid option.")
+
     
 def main():
     """
     Run all the application functions
     """
     location = location_input()
-    activity, climbing = activity_input()
     weather, temp, error_code = get_weather(location)
-
+    display_weather_basic(weather, temp)
+    
     # print(weather)
     # print(temp)
     # print(location)
