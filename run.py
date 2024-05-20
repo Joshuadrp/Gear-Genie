a59ed0c49d8488f552769d9c2b66408b34f91166
Joshua Rodriguez <joshuadrp@hotmail.com>
2024-05-20 16:36:49 +0000

    add fetch_gear_data function and update activity_input function to return activity type and column index

--- run.py
+++ run.py
@@ -42,6 +42,23 @@ def get_weather():
         # print(error_code)
         # print(weather_data.json())
 
+def display_weather_basic(weather, temp):
+    """
+    Displays basic weather info for user and let user choose to continue with app or terminate it.
+    """
+    print(f"Weather is currently {weather}, with temperature of {temp}.\nAfter reviewing the weather data, do you wish to continue?\n1.Yes\n2.No")
+    while True:
+        user_agree = input("Please choose: ")
+        if(user_agree.lower() == "yes"):
+            activity_input()
+            return 
+        elif(user_agree.lower() == "no"):
+            print("Program will be terminated.")
+            return
+        else:
+            print("Please select a valid option.")
+
+
 def activity_input():
     """
     Gets users location and acivity
@@ -51,52 +68,54 @@ def activity_input():
         
         activity = input("Please enter outdoor activity: ")
         climbing = None
+        column_index = None
 
-        if activity == "1":
+        if activity.lower() == "climbing":
             while True:
-                print("What type of climbing?\n1.Trad\n2.Sport\n3.Boulder")
+                print("What type of climbing?\n1.Trad Climbing\n2.Sport Climbing\n3.Bouldering")
                 climbing = input("Please choose: ")
-                if climbing == "1":
+                if climbing.lower() == "trad climbing":
                     print("You need trad gear.\n")
-                    return activity, climbing
-                elif climbing == "2":
+                    column_index = 0 #this will only give the basic gear, need to update it later. 
+                    return climbing, column_index
+                elif climbing.lower() == "sport climbing":
                     print("You need sport gear.\n")
-                    return activity, climbing
-                elif climbing == "3":
+                    column_index = 0 #this will only give the basic gear, need to update it later.
+                    return climbing, column_index
+                elif climbing.lower() == "bouldering":
                     print("You need boulder gear.\n")
-                    return activity, climbing
+                    column_index = 0 #this will only give the basic gear, need to update it later.
+                    return climbing, column_index
                 else:
                     print("Please select the type of climbing from the options.\n")
             
-        elif activity == "2":
+        elif activity.lower() == "hiking":
             print("You need hiking gear.\n")
-            return activity, climbing
+            column_index = 0 #this will only give the basic gear, need to update it later.
+            return column_index, activity
         else: 
             print("Please choose one of the activities above.\n")
 
-def display_weather_basic(weather, temp):
-    """
-    Displays basic weather info for user and let user choose to continue with app or terminate it.
-    """
-    print(f"Weather is currently {weather}, with temperature of {temp}.\nAfter reviewing the weather data, do you wish to continue?\n1.Yes\n2.No")
-    while True:
-        user_agree = input("Please choose: ")
-        if(user_agree == "1"):
-            activity_input()
-            return 
-        elif(user_agree == "2"):
-            print("Program will be terminated.")
-            return
-        else:
-            print("Please select a valid option.")
-
     
+
+def fetch_gear_data(user_activity, column_index):
+    """
+    Fetch data for hiking and climbing activities.
+    """
+    gear = SHEET.worksheet(f"{user_activity}").get_all_values()
+    column_data = [row[column_index] for row in gear]
+    return column_data
+
 def main():
     """
     Run all the application functions
     """
     weather, temp = get_weather()
     display_weather_basic(weather, temp)
+    
+    # column_index, user_activity = activity_input()
+    # fetched_gear = fetch_gear_data(user_activity, column_index)
+    # print(fetched_gear)
     
     # print(weather)
     # print(temp)
